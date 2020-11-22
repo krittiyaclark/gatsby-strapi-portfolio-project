ca3f0eaea63cecacb33802c8d48091685cfe39e6
krittiyaclark <krittiyaclark@gmail.com>
2020-11-22 16:08:15 -0500

    Added the hero component

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,14 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `${__dirname}/src/assets/`,
+      },
+    },
+  ],
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,10 +3,43 @@ import Image from "gatsby-image"
 import { Link } from "gatsby"
 import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/socialLinks"
-// ...GatsbyImageSharpFluid
 
+const query = graphql`
+  {
+    file(relativePath: { eq: "hero-img.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 const Hero = () => {
-  return <h2>hero component</h2>
+  // const data = useStaticQuery(query)
+  // Destructor from data
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query)
+
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div className="underline"></div>
+          <h1>I'm John</h1>
+          <h4>Freelance web and mobile Developer</h4>
+          <Link to="/contact" className="btn">
+            contact me
+          </Link>
+          <SocialLinks />
+        </article>
+        <Image fluid={fluid} className="hero-img" />
+      </div>
+    </header>
+  )
 }
 
 export default Hero
