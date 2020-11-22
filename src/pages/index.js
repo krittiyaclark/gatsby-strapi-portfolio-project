f8dcd8ba35f408e2f99de242d6558539fb514558
krittiyaclark <krittiyaclark@gmail.com>
2020-11-22 09:10:27 -0500

    Project setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
-
-export default function Home() {
-  return <div>Hello world!</div>
+import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import Hero from "../components/Hero"
+import Services from "../components/Services"
+import Jobs from "../components/Jobs"
+import Projects from "../components/Projects"
+import Blogs from "../components/Blogs"
+export default () => {
+  return <h2>index page</h2>
 }
+// ...GatsbyImageSharpFluid
