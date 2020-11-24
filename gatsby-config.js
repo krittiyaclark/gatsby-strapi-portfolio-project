f039aeff4ed84801d4100ffe8bd2fc00da35c07f
krittiyaclark <krittiyaclark@gmail.com>
2020-11-24 14:44:36 -0500

    Added the experience component

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,11 +8,22 @@ module.exports = {
   /* Your site config here */
   plugins: [
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-source-strapi`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `assets`,
         path: `${__dirname}/src/assets/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `http://localhost:1337`,
+        queryLimit: 2000, // Default to 100
+        contentTypes: [`jobs`, `projects`, `blogs`],
+        singleTypes: [`about`],
       },
     },
   ],
--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,11 +1,73 @@
-import React from "react"
+import React, { useState } from "react"
 import Title from "./Title"
 import { FaAngleDoubleRight } from "react-icons/fa"
 import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
 
+const query = graphql`
+  {
+    allStrapiJobs(sort: { fields: strapiId, order: DESC }) {
+      nodes {
+        strapiId
+        company
+        date
+        position
+        desc {
+          id
+          name
+        }
+      }
+    }
+  }
+`
+
 const Jobs = () => {
-  return <h2>jobs component</h2>
+  const data = useStaticQuery(query)
+  const {
+    allStrapiJobs: { nodes: jobs },
+  } = data
+  const [value, setValue] = useState(0)
+  const { company, date, position, desc } = jobs[value]
+  // console.log(company, date, position, desc)
+
+  return (
+    <section className="section jobs">
+      <Title title="experience" />
+      <div className="jobs-center">
+        <div className="btn-container">
+          {jobs &&
+            jobs.map((job, index) => {
+              return (
+                <button
+                  onClick={() => setValue(index)}
+                  key={job.strapiId}
+                  className={`job-btn ${index === value && "active-btn"}`}
+                >
+                  {job.company}
+                </button>
+              )
+            })}
+        </div>
+        <article className="job-info">
+          <h3>{position}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{date}</p>
+          {desc &&
+            desc.map(item => {
+              return (
+                <div key={item.id} className="job-desc">
+                  <FaAngleDoubleRight className="job-icon" />
+                  <p>{item.name}</p>
+                </div>
+              )
+            })}
+        </article>
+      </div>
+      <Link to="/about" className="btn center-btn">
+        More info
+      </Link>
+    </section>
+  )
 }
 
 export default Jobs
