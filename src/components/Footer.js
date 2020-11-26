e4ba0ac0276de28b287e6e7085f4c1fd1468aa84
krittiyaclark <krittiyaclark@gmail.com>
2020-11-25 20:12:22 -0500

    Added the footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,18 @@
 import React from "react"
 import SocialLinks from "../constants/socialLinks"
+
 const Footer = () => {
-  return <h2>footer component</h2>
+  return (
+    <footer className="footer">
+      <>
+        <SocialLinks styleClass="footer-links"></SocialLinks>
+        <h4>
+          copyright&copy;{new Date().getFullYear()}
+          <span>Portfolio</span> all rights resvered
+        </h4>
+      </>
+    </footer>
+  )
 }
 
 export default Footer
